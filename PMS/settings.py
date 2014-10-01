0e303b5a8008247abeaadde935fea2f73ee054c5
Ricky <ricky@RickydeMac-mini.local>
2014-10-01 14:11:43 +0800

    Initial commit

---
+++ PMS/settings.py
@@ -0,0 +1,125 @@
+"""
+Django settings for PMS project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'e6@pk*2q^*^f&kjefabuu78w+e34kqj$60*@tfdim8ngnm5u@1'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'crispy_forms',
+    'bootstrap_admin',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'bootstrap3',
+    'awesome_bootstrap',
+    'bootstrap3_datetime',
+    'south',
+    #'userena',
+    #'guardian',
+    #'easy_thumbnails',
+    'rest_framework',
+    'PmsApp',
+    #'accounts',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'PMS.urls'
+
+WSGI_APPLICATION = 'PMS.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR,'templates')]
+
+'''
+SOUTH_MIGRATION_MODULES = {
+    'easy_thumbnails': 'easy_thumbnails.south_migrations',
+}
+
+#============= Userena ====================
+AUTHENTICATION_BACKENDS = (
+    'userena.backends.UserenaAuthenticationBackend',
+    'guardian.backends.ObjectPermissionBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+ANONYMOUS_USER_ID = -1
+
+AUTH_PROFILE_MODULE = 'accounts.MyProfile'
+
+LOGIN_REDIRECT_URL = '/accounts/%(username)s/'
+LOGIN_URL = '/accounts/signin/'
+LOGOUT_URL = '/accounts/signout/'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'rickycm@gmail.com'
+EMAIL_HOST_PASSWORD = 'olag2ken'
+
+#============= Userena ====================
+'''
---
+++ PmsApp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+P_TYPE = [(1, u'Apartment'), (2, u'House'), (3, u'Villa')]
+P_STATUS = [(1, u'Empty'), (2, u'Rented'), (3, u'')]
+
+
+class tenantInfo(models.Model):
+    t_name = models.CharField(max_length=200, blank=True, null=True)
+    t_phone = models.CharField(max_length=20, blank=True, null=True)
+    t_address = models.CharField(max_length=20, blank=True, null=True)
+    t_email = models.EmailField(max_length=1000, verbose_name='e-mail', blank=True, null=True)
+
+class property(models.Model):
+    p_name = models.CharField(max_length=200, blank=True, null=True)
+    p_type = models.IntegerField(choices=P_TYPE, default=1, blank=True, null=True)
+    p_owner = models.ForeignKey(User, related_name='owner')
+    p_manager = models.ForeignKey(User,related_name='manager', blank=True, null=True)
+    p_tenant = models.ForeignKey(tenantInfo, blank=True, null=True)
+    p_area = models.IntegerField(blank=True, null=True)
+    p_buildtime = models.DateField(blank=True, null=True)
+    p_status = models.IntegerField(choices=P_STATUS, default=1, blank=True, null=True)
+'''
+    p_rentHistory = models.TextField(max_length=2000, blank=True, null=True)
+    p_phone = models.CharField(max_length=20, blank=True, null=True)
+    p_Econtact = models.CharField(max_length=20, blank=True, null=True)
+    p_email = models.EmailField(max_length=1000, verbose_name='e-mail', blank=True, null=True)
+    p_conception = models.DateField(blank=True, null=True)
+    p_addtime = models.DateTimeField(auto_now=True, blank=True, null=True)
+    doctor_id = models.CharField(max_length=20, blank=True, null=True)
+'''
