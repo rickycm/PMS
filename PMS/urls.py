a2e85a710b46ad13ec72dd1374d9cd90c5e225ba
Ricky <ricky@RickydeMac-mini.local>
2014-10-09 23:33:56 +0800

    引入base.html，搞定CSS路径有问题。添加Static文件路径配置

--- PMS/urls.py
+++ PMS/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from PmsApp.views import *
+from PMS import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'PMS.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_PATH}),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', index),
